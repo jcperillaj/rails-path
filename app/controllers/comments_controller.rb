387da17d0e5aa01f0c7cb1e20f033c213f559c41
Juan Camilo Perilla Jaramillo <juan.perilla.kommit@gmail.com>
2021-04-16 08:17:18 -0500

    Modified project folder structure

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.build(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to article_path(@article) }
+      else
+        format.html { redirect_to root_path, alert: @comment.errors.messages }
+      end
+    end
+
+  end
+
+  def destroy
+     @article = Article.find(params[:article_id])
+     @comment = @article.comments.find(params[:id])
+     @comment.destroy
+     redirect_to article_path(@article)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body, :status)
+    end
+end
