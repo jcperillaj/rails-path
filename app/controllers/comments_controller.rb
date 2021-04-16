bad42f8ba8a80544465a290481a765bfd24d61dd
Juan Camilo Perilla Jaramillo <juan.perilla.kommit@gmail.com>
2021-04-16 16:57:36 -0500

    Corrections to styles

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: 'dh', password: 'secret', only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
@@ -15,10 +15,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-     @article = Article.find(params[:article_id])
-     @comment = @article.comments.find(params[:id])
-     @comment.destroy
-     redirect_to article_path(@article)
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
   end
 
   private
